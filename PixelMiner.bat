6d3a2a8cd7d453db1bbb85be0d8c4656e547be4e
Fifty Walrus <fiftywalrus@gmail.com>
2024-01-17 01:18:03 +0200

    Revert "Merge branch 'main' of https://github.com/PixelMineStudio/PixelMiner"
    This reverts commit 20fe9303a1c49af938b22c3155497ef508413e56, reversing
    changes made to 6d16c3c23f043313ddd21cf9fb15ea8e48bab0d7.

--- PixelMiner.bat
+++
@@ -1,2 +0,0 @@
-python PixelMiner.py
-pause
--- PixelMiner_Build.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller --noconfirm --onefile --icon="assets/PixelMiner.ico" --console --add-data "scripts;scripts" --add-data "assets;assets" --add-data "Version_Mappings;Version_Mappings" PixelMiner.py
-pause
